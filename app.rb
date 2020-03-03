83cc321a7bd68ae40477f18417575e99663f77b3
pdinc <pavan.nyama@gmail.com>
2020-03-03 02:46:33 +0000

    March 2 in-class work

--- app.rb
+++ app.rb
@@ -17,6 +17,11 @@ after { puts; }                                                                 
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
 
 get "/" do
     puts events_table.all
@@ -28,6 +33,7 @@ get "/events/:id" do
     @event = events_table.where(id: params[:id]).to_a[0]
     @rsvps = rsvps_table.where(event_id: @event[:id])
     @going_count = rsvps_table.where(event_id: @event[:id]).sum(:going)
+    @users_table = users_table
     view "event"
 end
 
@@ -37,6 +43,12 @@ get "/events/:id/rsvps/new" do
 end
 
 get "/events/:id/rsvps/create" do
+    puts params
+    @event = events_table.where(id: params[:id]).to_a[0]
+    rsvps_table.insert(event_id: params["id"], 
+                        going: params["going"], 
+                        user_id: session["user_id"], 
+                        comments: params["comments"])
     view "create_rsvp"
 end
 
@@ -44,8 +56,11 @@ get "/users/new" do
     view "new_user"
 end
 
-get "/users/create" do
+post "/users/create" do
     puts params
+    users_table.insert(name: params["name"],
+                email: params["email"],
+                password: BCrypt::Password.create(params["password"]))
     view "create_user"
 end
 
@@ -53,11 +68,26 @@ get "/logins/new" do
     view "new_login"
 end
 
-get "/logins/create" do
+post "/logins/create" do
     puts params
-    view "create_login"
+    email_address = params["email"]
+    password = params["password"]
+    @user = users_table.where(email: email_address).to_a[0]
+    
+    if @user
+        puts @user
+        if BCrypt::Password.new(@user[:password]) == password
+            session["user_id"] = @user[:id]
+            view "create_login"
+        else    
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
 end
 
 get "/logout" do
+    session["user_id"] = nil
     view "logout"
 end
